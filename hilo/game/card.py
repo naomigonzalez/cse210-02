87c63d0431281bff0510a72affe06919be82b9c3
naomigonzalez <gon21043@byui.edu>
2022-11-12 02:30:56 -0600

    W2 hilo

---
+++ hilo/game/card.py
@@ -0,0 +1,9 @@
+import random
+
+class Card:
+    def __init__(self):
+        self.value = 0
+
+    def draw(self):
+        self.value = random.randint(1, 13)
+        return self.value
--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,85 +1,56 @@
-from game.hilo import Hilo
-
+from game.card import Card
 
 class Dealer:
-    """A person who directs the game. 
-    
-    The responsibility of a Dealer is to control the sequence of play.
-
-    Attributes:
-        dice (List[Die]): A list of Die instances.
-        is_playing (boolean): Whether or not the game is being played.
-        score (int): The score for one round of play.
-        total_score (int): The score for the entire game.
-    """
 
     def __init__(self):
-        """Constructs a new Director.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
-        self.dice = []
         self.is_playing = True
-        self.score = 300 
-        self.total_score = 0
-
-        for i in range(5):
-            die = Die()
-            self.dice.append(die)
+        self.starting_points = 300
+        self.card_guess = ""
+        self.next_card = ""
+        self.card = Card()
+        self.current_card = self.card.draw()
 
     def start_game(self):
-        """Starts the game by running the main game loop.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
-        while self.is_playing:
-            self.get_inputs()
+        while self.is_playing and self.starting_points > 0:
+            self.get_input()
             self.do_updates()
             self.do_outputs()
 
-    def get_inputs(self):
-        """Ask the user if they want to roll.
+    def get_input(self):
+        print(f"The card is: {self.current_card}")
+        self.card_guess = input("Higher or Lower? [h/l]: ")
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        roll_dice = input("Roll dice? [y/n] ")
-        self.is_playing = (roll_dice == "y")
-       
+        while (self.card_guess != "h") and (self.card_guess != "l"):
+            print("You can only type 'h' or 'l'")
+            self.card_guess = input("Higher or Lower? [h/l]: ")
+
     def do_updates(self):
-        """Updates the player's score.
+        self.next_card = self.card.draw()
+        while self.next_card == self.current_card:
+            self.next_card = self.card.draw()
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.is_playing:
-            return 
+        if self.current_card < self.next_card:
+            answer = "h"
+        elif self.current_card > self.next_card:
+            answer = "l"
 
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            die.roll()
-            self.score += die.points 
-        self.total_score += self.score
+        if self.card_guess == answer:
+            self.starting_points += 100
+        else:
+            self.starting_points -= 75
 
     def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
+        print(f"Next card was: {self.next_card}")
+        print(f"Your score is: {self.starting_points}")
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.is_playing:
-            return
-        
-        values = ""
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            values += f"{die.value} "
 
-        print(f"You rolled: {values}")
-        print(f"Your score is: {self.total_score}\n")
-        # modified this part 
-        # self.is_playing == self.score > 0 
-        self.is_playing = (self.score > 0)
-        self.score = 0
+        if self.starting_points > 0:
+            keep_playing = input("Play again? [y/n]: ")
+
+            while (keep_playing != "y") and (keep_playing != "n"):
+                print("You can only type 'y' or 'n'.")
+                keep_playing = input("Play again? [y/n]: ")
+
+            self.is_playing = (keep_playing == "y")
+        print()
+        self.current_card = self.next_card  
